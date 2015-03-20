ad9855182809df5db8afe6963322083392afd758
Brian Crucitti <bcrucitti@gmail.com>
2015-03-20 15:23:45 -0500

    add dist files

--- grunt/clean.js
+++ grunt/clean.js
@@ -1,3 +1,4 @@
 module.exports = {
-    coverage: [ "coverage/" ]
+    coverage: [ "coverage/" ],
+    dist: [ "dist/" ]
 };
---
+++ grunt/pkg.js
@@ -0,0 +1,5 @@
+module.exports = function ( grunt )
+{
+    "use strict";
+    return grunt.file.readJSON( "package.json" );
+};
